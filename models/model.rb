b407e62f79424a8cf92305a05ad004fef4cdec22
Sebastian Andrade <sebastiana7420@hstat.org>
2019-03-19 04:30:04 +0000

    add more

--- models/model.rb
+++ models/model.rb
@@ -1,5 +1,6 @@
 require 'net/http'
 require 'json'
+# require 'paralleldots'
 require 'pp'
 
 def get_gif(mood)
@@ -9,12 +10,11 @@ def get_gif(mood)
     result = JSON.parse(response)
     result["data"]["embed_url"]
 end
-# def get_antonym(mood)
-    # url = "https://www.dictionaryapi.com/api/v3/references/thesaurus/json/happy?key=0c1c42b3-ad1e-46ba-ace6-6e297eb7df07"
-    # uri = URI(url)
-    # response = Net::HTTP.get(uri)
-    # result = JSON.parse(response)
-    # puts result
-# end
-#IGNORE THIS ^ I WAS TRYING TO USE A SECOND API TO GET AN ANTONYM OF THE USERS INPUT
+
+# text= "The movie made me sad"
+# response = emotion(text, lang_code= "en")
+# result = (response)
+# puts response["emotion"]["emotion"]
+
+
 # puts get_gif("happy")
