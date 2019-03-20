b73728260b10862b9a91bea29694d3f6419095dc
Sebastian Andrade <sebastiana7420@hstat.org>
2019-03-20 23:43:41 +0000

    get the second API to work on my own time

--- models/model.rb
+++ models/model.rb
@@ -3,17 +3,20 @@ require 'json'
 require 'paralleldots'
 require 'pp'
 
-def get_gif(mood)
-    url = "http://api.giphy.com/v1/gifs/random?tag=" + @user_feeling + "&api_key=" + ENV['key'] + "&limit=1"
+def get_gif()
+    url = "http://api.giphy.com/v1/gifs/random?tag=" + get_mood() + "&api_key=rzuGbBtNPd8X0vnCyTPxrqRnFsnL25eA&limit=1"
     uri = URI(url)
     response = Net::HTTP.get(uri)
     result = JSON.parse(response)
     result["data"]["embed_url"]
 end
 
-@text= @user_sentence
+def get_mood()
+@text= params[:feelings]
 response=emotion(@text, lang_code= "en")
-@user_feeling = ["emotion"]["emotion"]
+response["emotion"]["emotion"]
+end
 
+# puts get_mood()
 
 # puts get_gif("happy")
